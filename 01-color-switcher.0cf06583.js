d1ad391f561c879e417a77826f2efb20d43f3799
BorschVS <129619261+BorschVS@users.noreply.github.com>
2023-11-14 19:54:33 +0000

    Deploying to gh-pages from @ BorschVS/goit-js-hw-09@356c8870807f80805431aa99d21379f301a6e735 🚀

---
+++ 01-color-switcher.0cf06583.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]")};let n=null;t.btnStop.disabled=!1;let e=!1;t.btnStart.addEventListener("click",(function(){if(e)return;e=!0,n=setInterval((()=>{t.btnStop.disabled=!1,t.btnStart.disabled=!0,document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.btnStop.addEventListener("click",(function(){e=!1,t.btnStop.disabled=!0,t.btnStart.disabled=!1,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.0cf06583.js.map
